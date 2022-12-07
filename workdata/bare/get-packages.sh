10a82030e9748749f367473d1a9c47fd1bbb85eb
d10n <d10n@redhat.com>
2022-12-07 02:50:29 -0500

    Shorten index data paths

--- workdata/bare/get-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-#wget --recursive --level inf --xattr --no-parent --span-hosts --domains files.pythonhosted.org --convert-links 'https://pypi.org/simple/bottle/'
-#wget --recursive --level inf --xattr --no-parent --span-hosts --domains files.pythonhosted.org --convert-links 'https://pypi.org/simple/isodate/'
-#wget --recursive --level inf --xattr --no-parent --span-hosts --domains files.pythonhosted.org --convert-links 'https://pypi.org/simple/iniconfig/'
-#wget --recursive --level inf --xattr --no-parent --span-hosts --domains files.pythonhosted.org --convert-links --content-on-error 'https://pypi.org/simple/nonexistantpackagehere/'
-wget --xattr --convert-links --content-on-error 'https://pypi.org/simple/'
